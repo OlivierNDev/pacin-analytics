434afa5706fcbac99f2e045aba19e95134daee11
Olivier Niyonshima <161640657+OlivierNDev@users.noreply.github.com>
2026-09-14 14:54:37 -0400

    feat: DB schema, models, auth, database, utils

---
+++ api/database.py
@@ -0,0 +1,17 @@
+import os
+import asyncpg
+from contextlib import asynccontextmanager
+
+_pool = None
+
+async def get_pool():
+    global _pool
+    if _pool is None:
+        _pool = await asyncpg.create_pool(os.environ['DATABASE_URL'], min_size=2, max_size=10)
+    return _pool
+
+async def close_pool():
+    global _pool
+    if _pool:
+        await _pool.close()
+        _pool = None
